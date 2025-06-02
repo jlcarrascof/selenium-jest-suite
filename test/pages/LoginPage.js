2cf5f4457e5a63540cdb8941889a0ef8fca7e0a3
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-02 13:06:41 -0400

    Add LoginPage class with methods for form interaction and validation

---
+++ test/pages/LoginPage.js
@@ -0,0 +1,51 @@
+import { By } from 'selenium-webdriver';
+
+export class LoginPage {
+  /**
+   * @param {WebDriver} driver
+   */
+  constructor(driver) {
+    this.driver = driver;
+    this.selectors = {
+      emailInput: By.css('input[name="email"]'),
+      passwordInput: By.css('input[name="password"]'),
+      submitButton: By.css('button[type="submit"]'),
+      errorMessage: By.css('[data-testid="error-message"]'),
+      loader: By.css('[data-testid="loader"]'),
+    };
+  }
+
+  async enterEmail(email) {
+    const input = await this.driver.findElement(this.selectors.emailInput);
+    await input.clear();
+    await input.sendKeys(email);
+  }
+
+  async enterPassword(password) {
+    const input = await this.driver.findElement(this.selectors.passwordInput);
+    await input.clear();
+    await input.sendKeys(password);
+  }
+
+  async submitForm() {
+    await this.driver.findElement(this.selectors.submitButton).click();
+  }
+
+  async getErrorMessage() {
+    return await this.driver.findElement(this.selectors.errorMessage).getText();
+  }
+
+  async isSubmitButtonEnabled() {
+    const button = await this.driver.findElement(this.selectors.submitButton);
+    return await button.isEnabled();
+  }
+
+  async isLoaderVisible() {
+    try {
+      const loader = await this.driver.findElement(this.selectors.loader);
+      return await loader.isDisplayed();
+    } catch {
+      return false;
+    }
+  }
+}
