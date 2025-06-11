a2995f5869805ffe8159ebf81f9de7936155c2c8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-11 11:36:13 -0400

    Refactor LoginPage constructor to include baseUrl and implement open method for navigation

--- test/factories/pagesFactory.js
+++ test/factories/pagesFactory.js
@@ -1,6 +1,7 @@
 const LandingPage = require('../pages/LandingPage');
 const LoginPage = require('../pages/LoginPage');
 const NewAccountPage = require('../pages/NewAccountPage');
+const ProfilePage = require('../pages/ProfilePage');
 
 class PageFactory {
   static createPage(pageName, driver, baseUrl, timeout) {
@@ -8,7 +9,7 @@ class PageFactory {
       case 'landing':
         return new LandingPage(driver, baseUrl, timeout);
       case 'login':
-        return new LoginPage(driver, timeout);
+        return new LoginPage(driver, baseUrl, timeout);
       case 'newAccount':
         return new NewAccountPage(driver, baseUrl, timeout);
       case 'profile':
