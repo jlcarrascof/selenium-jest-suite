031968aa8e4c8e7f900dd0b1810d43f828f6cdc4
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-06-12 10:49:17 -0400

    Code base for begin the groups tests suite

---
+++ test/groups.spec.js
@@ -0,0 +1,50 @@
+const DriverFactory = require('./factories/driverFactory');
+const PageFactory = require('./factories/pagesFactory');
+const { By, until, Key } = require('selenium-webdriver');
+
+const TIMEOUT = 120000;
+const BASE_URL = 'https://login.harmonychurchsuite.com/tenant/user-signin?tenant=qa';
+const GROUPS_URL = 'https://qa.harmonychurchsuite.com/tenant/groups/index';
+const EXPECTED_URL = 'https://qa.harmonychurchsuite.com/404';
+const VALID_USERNAME = 'javier';
+const VALID_PASSWORD = '.qwerty123.';
+const INVALID_USERNAME = 'invalidUser';
+const INVALID_PASSWORD = 'invalidPass';
+const CURRENT_BROWSER = 'chrome';
+const DASHBOARD_TITLE_SELECTOR = 'h1.text-xl.font-semibold';
+
+let driver;
+let loginPage;
+let groupsPage;
+
+beforeAll(async () => {
+  const driverFactory = new DriverFactory(CURRENT_BROWSER, TIMEOUT);
+  driver = await driverFactory.initDriver();
+  loginPage = PageFactory.createPage('login', driver, BASE_URL, TIMEOUT);
+  groupsPage = PageFactory.createPage('groups', driver, BASE_URL, TIMEOUT);
+});
+
+afterAll(async () => {
+  if (driver) {
+    await driver.quit();
+  }
+});
+
+describe('Test Suite: User Profile Functionality of Harmony Church', () => {
+  test('TC-001: Valid credentials should login successfully', async () => {
+    await loginPage.open();
+    await loginPage.enterUsername(VALID_USERNAME);
+    await loginPage.enterPassword(VALID_PASSWORD);
+    await loginPage.clickSubmit();
+
+    const dashboardElement = await driver.wait(
+      until.elementLocated(By.css(DASHBOARD_TITLE_SELECTOR)),
+      TIMEOUT
+    );
+    const actualResult = await dashboardElement.getText();
+    const expectedResult = /dashboard/i;
+
+    expect(actualResult).toMatch(expectedResult);
+  });
+
+});
---
+++ test/pages/GroupsPage.js
@@ -0,0 +1,108 @@
+// tests/pages/LoginPage.js
+const { By, until, Key } = require('selenium-webdriver');
+const WAIT_TIME = 1000;
+
+class GroupsPage {
+  constructor(driver, baseUrl, timeout) {
+    this.driver = driver;
+    this.baseUrl = baseUrl;
+    this.timeout = timeout;
+
+    this.selectors = {
+      usernameInput: "input[placeholder='Enter your username']",
+      passwordInput: "input[placeholder='Enter your password']",
+      submitButton: "button[type='submit']",
+      profileIcon: 'div.menu-item[data-menu-item-toggle="dropdown"]',
+      logoutButton: '//button[contains(text(), "Log out")]',
+      appsButton: '//*[@id="header_container"]/div[3]/div[2]/button',
+      groupsOption: '//div[span[text()="Groups"]]',
+      myProfileLink: '//*[@id="header_container"]/div[3]/div[3]/div/div[2]/div[3]/div/a',
+      usersLink: '//span[normalize-space()="Users" and contains(@class, "menu-title")]',
+      rolesPermissionsLink: '//span[normalize-space()="Users" and contains(@class, "menu-title")]',
+      eventLogLink: '//span[normalize-space()="Event log" and contains(@class, "menu-title")]',
+      allNotificationsLink: '//span[normalize-space()="All notifications" and contains(@class, "menu-title")]',
+      roleNotificationsLink: '//span[normalize-space()="Role notifications" and contains(@class, "menu-title")]',
+      userNotificationsLink: '//span[normalize-space()="User notifications" and contains(@class, "menu-title")]',
+      languagesLink: '//span[normalize-space()="Languages" and contains(@class, "menu-title")]',
+      referenceDataLink: '//span[normalize-space()="Reference data" and contains(@class, "menu-title")]',
+      subscriptionLink: '//span[normalize-space()="Subscription" and contains(@class, "menu-title")]',
+    };
+  }
+
+  async enterUsername(username) {
+    const usernameField = await this.driver.findElement(
+      By.css(this.selectors.usernameInput)
+    );
+
+    await usernameField.clear();
+    await usernameField.sendKeys(username);
+  }
+
+  async enterPassword(password) {
+    const passwordField = await this.driver.findElement(
+      By.css(this.selectors.passwordInput)
+    );
+
+    await passwordField.clear();
+    await passwordField.sendKeys(password);
+  }
+
+  async clickSubmit() {
+    const submitBtn = await this.driver.wait(
+      until.elementLocated(By.css(this.selectors.submitButton)),
+      this.timeout
+    );
+
+    await this.driver.wait(until.elementIsVisible(submitBtn), this.timeout);
+    await this.driver.wait(until.elementIsEnabled(submitBtn), this.timeout);
+    await submitBtn.click();
+  }
+
+  async isSubmitButtonDisabled() {
+    const submitBtn = await this.driver.findElement(
+      By.css(this.selectors.submitButton)
+    );
+
+    return !(await submitBtn.isEnabled());
+  }
+
+  async clickProfileIcon() {
+    const profileIcon = await this.driver.wait(
+      until.elementLocated(By.css(this.selectors.profileIcon)),
+      this.timeout
+    );
+
+    await profileIcon.click();
+  }
+
+  async isLogoutButtonVisible() {
+    const logoutButton = await this.driver.wait(
+      until.elementLocated(By.xpath(this.selectors.logoutButton)),
+      this.timeout
+    );
+
+    return await this.driver.wait(until.elementIsVisible(logoutButton), this.timeout);
+  }
+
+  async clickLogout() {
+    const logoutButton = await this.driver.wait(
+      until.elementLocated(By.xpath(this.selectors.logoutButton)),
+      this.timeout
+    );
+
+    await logoutButton.click();
+    await this.driver.sleep(WAIT_TIME);
+  }
+
+  async isOnLoginPage() {
+    await this.driver.sleep(WAIT_TIME);
+
+    const currentUrl = await this.driver.getCurrentUrl();
+
+    return currentUrl.startsWith(this.baseUrl);
+  }
+
+
+}
+
+module.exports = GroupsPage;
