66956c0fb3b2e3d8080eeb526fad3951b4f9efeb
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-07-02 11:09:41 -0400

    Add userProfileTestSetup.js for user profile testing setup and configuration

---
+++ test/helpers/userProfileTestSetup.js
@@ -0,0 +1,53 @@
+const DriverFactory = require('../factories/driverFactory');
+const PageFactory = require('../factories/pagesFactory');
+
+const CONFIG = {
+  USERNAME: 'javier',
+  PASSWORD: '.qwerty123.',
+  INVALID_USERNAME: 'invalidUser',
+  INVALID_PASSWORD: 'invalidPass',
+  BASE_URL: 'https://login.harmonychurchsuite.com/tenant/user-signin?tenant=qa',
+  DASHBOARD_TITLE_SELECTOR: 'h1.text-xl.font-semibold',
+  INVALID_LOGIN_MESSAGE: 'Invalid credentials.',
+  EXPECTED_URL: 'https://qa.harmonychurchsuite.com/404',
+  GROUPS_URL: 'https://qa.harmonychurchsuite.com/tenant/groups/index',
+  TIMEOUT: 15000,
+
+  // URLs for 404 pages
+  MYPROFILE_URL: 'https://qa.harmonychurchsuite.com/404',
+  ROLES_PERMISSIONS_URL: 'https://qa.harmonychurchsuite.com/404',
+  USERS_URL: 'https://qa.harmonychurchsuite.com/404',
+  EVENT_LOG_URL: 'https://qa.harmonychurchsuite.com/404',
+  ALL_NOTIFICATIONS_URL: 'https://qa.harmonychurchsuite.com/404',
+  ROLE_NOTIFICATIONS_URL: 'https://qa.harmonychurchsuite.com/404',
+  USER_NOTIFICATIONS_URL: 'https://qa.harmonychurchsuite.com/404',
+  LANGUAGES_URL: 'https://qa.harmonychurchsuite.com/404',
+  REFERENCE_DATA_URL: 'https://qa.harmonychurchsuite.com/404',
+  SUBSCRIPTION_DATA_URL: 'https://qa.harmonychurchsuite.com/404',
+};
+
+let driver, loginPage, profilePage;
+
+const initPages = async () => {
+  const driverFactory = new DriverFactory(global.testConfig.currentBrowser, global.testConfig.timeout);
+  driver = await driverFactory.initDriver();
+
+  loginPage = PageFactory.createPage('login', driver, CONFIG.BASE_URL, global.testConfig.timeout);
+  profilePage = PageFactory.createPage('profile', driver, CONFIG.BASE_URL, global.testConfig.timeout);
+
+  return { driver, loginPage, profilePage };
+};
+
+module.exports = {
+  CONFIG,
+  initPages,
+  get driver() {
+    return driver;
+  },
+  get loginPage() {
+    return loginPage;
+  },
+  get profilePage() {
+    return profilePage;
+  }
+};
